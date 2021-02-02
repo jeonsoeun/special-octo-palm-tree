6d6b8ac2a7e6dc3fb3be245756e19088bf1bf4d2
jeonsoeun <jse525@naver.com>
2021-02-02 15:21:20 +0900

    useState사용해서 동적데이터 변경

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,17 +4,24 @@ import { html, React } from "./lib.js";
 import TodoItem from "./TodoItem.js";
 
 const Todo = () => {
-  const todoList = React.useState();
-  const todoList = [{ text: "abcd", date: "2021-01-30 12:00:00" }];
+  const [todoList, setTodoList] = React.useState([
+    { text: "abcd", date: "2021-01-30 12:00:00" },
+  ]);
   const onclick = (e) => {
     const inputEl = document.querySelector("#todo-input");
     if (inputEl.value) {
       const date = new Date();
-      const dateStr = `${date.getFullYear()}-${
+      const dateStr = `${set2Digits(date.getFullYear())}-${set2Digits(
         date.getMonth() + 1
-      }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-      todoList.push({ text: inputEl.value, date: dateStr });
+      )}-${set2Digits(date.getDate())} ${set2Digits(
+        date.getHours()
+      )}:${set2Digits(date.getMinutes())}:${set2Digits(date.getSeconds())}`;
+      setTodoList([...todoList, { text: inputEl.value, date: dateStr }]);
+      console.log(todoList);
     }
+  };
+  const set2Digits = (num) => {
+    return num >= 10 ? `${num}` : `0${num}`;
   };
   return html`<div class="todo">
     <h2>TODO LIST</h2>
