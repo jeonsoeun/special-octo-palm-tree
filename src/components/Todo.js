fb691cbcc6e8c5c1c1d505e5474c41fe057e2ab3
jeonsoeun <jse525@naver.com>
2021-02-02 14:38:22 +0900

    왜 추가가 안될까..

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,20 @@
 /** @format */
 
-import { html } from "./lib.js";
+import { html, React } from "./lib.js";
 import TodoItem from "./TodoItem.js";
 
 const Todo = () => {
-  const todoList = [{ text: "abcd", date: "2021-01-30" }];
+  const todoList = React.useState();
+  const todoList = [{ text: "abcd", date: "2021-01-30 12:00:00" }];
   const onclick = (e) => {
-    const inputStr = document.querySelector("#todo-list").value;
-    todoList.push(inputStr);
-    console.log(inputStr);
+    const inputEl = document.querySelector("#todo-input");
+    if (inputEl.value) {
+      const date = new Date();
+      const dateStr = `${date.getFullYear()}-${
+        date.getMonth() + 1
+      }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      todoList.push({ text: inputEl.value, date: dateStr });
+    }
   };
   return html`<div class="todo">
     <h2>TODO LIST</h2>
@@ -16,11 +22,9 @@ const Todo = () => {
       <input type="string" id="todo-input" />
       <button onClick=${onclick}>입력</button>
     </div>
-    <div className="todo-list">
-      ${todoList.map((v) => {
-        return html`<${TodoItem} todoItem=${v} />`;
-      })}
-    </div>
+    <ul className="todo-list">
+      ${todoList.map((v) => html`<${TodoItem} todoItem=${v} />`)}
+    </ul>
   </div>`;
 };
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,11 +3,11 @@
 import { html } from "./lib.js";
 
 const TodoList = ({ todoItem }) => {
-  return html` <div class="todo-item">
+  return html` <li class="todo-item">
     <input type="checkbox" name="todo-check" />
     <span className="todo-text">${todoItem.text}</span>
     <span className="todo-date">${todoItem.date}</span>
-  </div>`;
+  </li>`;
 };
 
 export default TodoList;
